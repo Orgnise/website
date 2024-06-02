6f50e2e402726c45fd5847963cba9bbc549dd34f
Sonu Sharma <sonu.sharma045@gmail.com>
2024-06-02 17:56:03 +0530

    Update build command

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,9 +11,9 @@ export const config = {
      * 3. /_proxy/ (special page for OG tags proxying)
      * 4. /_static (inside /public)
      * 5. /_vercel (Vercel internals)
-     * 6. Static files (e.g. /favicon.ico, /sitemap.xml, /robots.txt, etc.)
+     * 6. /favicon.ico, /sitemap.xml, /robots.txt (static files)
      */
-    "/((?!api/|_next/|_proxy/|_static|_vercel|[\\w-]+\\.\\w+).*)",
+    "/((?!api/|_next/|_proxy/|_static|_vercel|favicon.ico|sitemap.xml|robots.txt).*)",
   ],
 };
 
